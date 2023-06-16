bb06cf323049c3024458bbc171878fcf9997d137
Julia Aseeva <juliata0506@gmail.com>
2023-06-17 00:42:12 +0300

    build settings update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 const mode = process.env.NODE_ENV;
 const isProd = mode === 'production';
 
 export default defineConfig({
   mode,
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    sourcemap: isProd,
+    emptyOutDir: isProd,
+  },
   plugins: [react()],
 });
