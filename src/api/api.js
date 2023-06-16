16058a908c0bbbca3d937cd201039f31e4f0b5ac
Julia Aseeva <juliata0506@gmail.com>
2023-06-16 23:54:17 +0300

    gh-pages setup

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,13 @@
 import { dateFormats } from "../constants/date"
 import { getDayInfo } from "./apiService"
 
+const base_url = 'https://api.nasa.gov/neo/rest/v1/feed'
 const api_key = "PXjG2k4gTiQT1uLnemaLCDAX3RDa7jRbL69WIROx"
 
 export function getNeoByDate(date, onGet){
     const formattedDate = date.format(dateFormats.defaultApiFormat)
 
-    fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${formattedDate}&end_date=${formattedDate}&api_key=${api_key}`)
+    fetch(`${base_url}?start_date=${formattedDate}&end_date=${formattedDate}&api_key=${api_key}`)
     .then(res=>res.json())
     .then((data) => {
         const dayInfo = getDayInfo(data.near_earth_objects[formattedDate])
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const mode = process.env.NODE_ENV;
+const isProd = mode === 'production';
+
 export default defineConfig({
+ mode,
+ build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    sourcemap: isProd,
+    emptyOutDir: isProd,
+    rollupOptions: {
+    input: {
+      react: path.resolve(__dirname, 'src')
+    }
+  },
   plugins: [react()],
-})
+});
