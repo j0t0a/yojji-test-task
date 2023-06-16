33a8f5e6bc892e193862c7e408a76438d670055b
Julia Aseeva <juliata0506@gmail.com>
2023-06-17 00:03:07 +0300

    cross-env adding

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,5 @@ const isProd = mode === 'production';
 
 export default defineConfig({
   mode,
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    sourcemap: isProd,
-    emptyOutDir: isProd,
-    rollupOptions: {
-      input: {
-        react: path.resolve(__dirname, 'src')
-      }
-    }
-  },
   plugins: [react()],
 });
